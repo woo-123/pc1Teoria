2e310f22580250039841a9a9f4b8ed10f05f1da8
Diego Chiroque <diego-al-18@hotmail.com>
2021-09-14 17:11:21 -0500

    hola

---
+++ Models/Registro.cs
@@ -0,0 +1,10 @@
+using System;
+namespace pc1Teoria.Models
+{
+    public class Registro
+    {
+        public string Nombre {get;set;}
+        public string Apellido {get;set;}
+        public int Id {get;set;}
+    }
+}
